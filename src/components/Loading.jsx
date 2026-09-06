7debfdd78ddefdd4b86724454b7955bd0f4c3186
Kunal <kunalkaushikgkk@gmail.com>
2026-09-07 00:00:51 +0530

    Create Loading.jsx

---
+++ src/components/Loading.jsx
@@ -0,0 +1,16 @@
+function Loading() {
+
+    return (
+        <div className="loading">
+
+            <div className="spinner"></div>
+
+            <p>
+                JanSahayak is searching documents...
+            </p>
+
+        </div>
+    );
+}
+
+export default Loading;
