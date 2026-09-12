084360403d535f270723f1d0988ff942fba486af
Kunal <kunalkaushikgkk@gmail.com>
2026-09-12 17:05:33 +0530

    Create Chat.jsx

---
+++ src/pages/Chat.jsx
@@ -0,0 +1,24 @@
+import ChatBox from "../components/ChatBox";
+
+function Chat() {
+
+    return (
+
+        <div className="page">
+
+            <h1>
+                Ask JanSahayak
+            </h1>
+
+            <p>
+                Ask questions about government
+                schemes and documents.
+            </p>
+
+            <ChatBox />
+
+        </div>
+    );
+}
+
+export default Chat;
